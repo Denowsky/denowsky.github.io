c2125d7fcb96be1ed34bd7cb4d4c32a30a568dbb
Denowsky <den.san404@gmail.com>
2024-04-10 19:16:24 +0300

    Portfolio 100%

---
+++ CreativeCult/assets/scripts/script.js
@@ -0,0 +1,6 @@
+document.getElementById("focusButton-discuss").addEventListener("click", () => {
+    document.getElementById("focus-discuss").focus();
+});
+document.getElementById("focusButton-send").addEventListener("click", () => {
+    document.getElementById("focus-send").focus();
+});
